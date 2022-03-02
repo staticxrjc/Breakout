2f3b34b6fb3067c1a5fdef1457ff71978278f72d
staticxrjc <staticxrjc@gmail.com>
2022-03-01 18:03:16 -0700

    SFML Working builds with CMAKE

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!!!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
